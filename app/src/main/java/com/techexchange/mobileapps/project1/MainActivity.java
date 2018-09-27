1750b5457f548af462a3bdae3305803540bd0069
Anita Calmday <calmday@calmday-macbookpro.roam.corp.google.com>
2018-09-27 15:43:44 -0700

    initial commit

---
+++ app/src/main/java/com/techexchange/mobileapps/project1/MainActivity.java
@@ -0,0 +1,20 @@
+package com.techexchange.mobileapps.project1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+
+}
